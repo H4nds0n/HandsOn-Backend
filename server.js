2559e86a2aa9e53be7cf75ee0894c4ff6af4d6cd
Dennis Leikauf <dennis.leikauf@gmail.com>
2023-10-18 16:04:15 +0200

    BAHHH

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import multer from 'multer'
 
 const app = express()
 
+const encoding = 'image/jpeg'
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage})
+
 app.use(cors())
-app.use(bodyParser.raw({ type: 'image/png' }));
-// app.use(bodyParser.json())
+app.use(bodyParser.raw({ type: encoding, limit: "50mb" }));
+app.use(bodyParser.urlencoded({extended: true, limit: "50mb"}))
+// multipart/form-data
 
 
 app.get('/', (req, res) => {
     res.send("Hell, World!")
 })
 
-app.post('/streaming', (req, res) => {
+app.post('/streaming', upload.single('blobData'), (req, res) => {
+    console.log(req)
     const buffer = req.body
     // const b = new Blob([req.body], {type: "image/jpg"})
-    const dataURI = `data:image/png;base64,${buffer.toString('base64')}`;
-    res.status(200).send('success')
-    console.log(req.body)
+    const dataURI = `data:${encoding};base64,${buffer.toString('base64')}`;
+    res.status(200).send(`<img src="${dataURI}">`)
 })
 
 app.listen(5000, () => {
