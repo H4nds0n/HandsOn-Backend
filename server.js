a00872abfdf8da6a2ab52890c87d540aee7043d8
Dennis Leikauf <dennis.leikauf@gmail.com>
2023-10-18 16:28:01 +0200

    usability

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-// import multer from 'multer'
-
 const app = express()
 
-const encoding = 'image/jpeg'
-// const storage = multer.memoryStorage();
-// const upload = multer({storage: storage})/
+const encoding = 'multipart/form-data'
+let counter = 0
 
 app.use(cors())
 app.use(bodyParser.raw({ type: encoding, limit: "50mb" }));
@@ -20,11 +17,13 @@ app.get('/', (req, res) => {
 })
 
 app.post('/streaming', (req, res) => {
-    console.log(req)
+    console.log(`Number requests: ${counter}`)
     const buffer = req.body
-    // const b = new Blob([req.body], {type: "image/jpg"})
     const dataURI = `data:${encoding};base64,${buffer.toString('base64')}`;
+    // res.status(200)
+    // Testing:
     res.status(200).send(`<img src="${dataURI}">`)
+    counter++
 })
 
 app.listen(5000, () => {
