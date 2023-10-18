42a5f7ab5dedde646186ba76a0518039b2c9def1
Dennis Leikauf <dennis.leikauf@gmail.com>
2023-10-18 15:28:25 +0200

    Works ig

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ import bodyParser from 'body-parser'
 const app = express()
 
 app.use(cors())
-// app.use(express.json())
-app.use(bodyParser.json())
+app.use(bodyParser.raw({ type: 'image/png' }));
+// app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.send("Hell, World!")
 })
 
 app.post('/streaming', (req, res) => {
-    console.log("Blob: ", req.body)
-    res.status(200).send(`<img src="${req.body}"></img>`)
+    const buffer = req.body
+    // const b = new Blob([req.body], {type: "image/jpg"})
+    const dataURI = `data:image/png;base64,${buffer.toString('base64')}`;
+    res.status(200).send('success')
+    console.log(req.body)
 })
 
 app.listen(5000, () => {
