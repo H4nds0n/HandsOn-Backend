0515160ab5e8733ff447d5d31df8e90ab9f9cd9c
Dennis Leikauf <dennis.leikauf@gmail.com>
2023-11-29 15:33:27 +0100

    AHHHHHHHHHHHHHHHHHHHHAHHHHHHHHHHHHHHHHH

--- pyenv/app.py
+++ pyenv/app.py
@@ -1,22 +1,29 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
 from getHands import getHands
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/streaming', methods=['POST'])
+@cross_origin()
 def streaming():
     if request.method == 'POST':
-        post_data = request.get_json()['base64']
+        post_data = request.get_json()['img'].split(',')[-1]
         try:
             letter, conf = getHands(post_data)
-        except e:
+            conf = conf.item()
+        except Exception as e:
             letter = ""
             conf = ""
             print(e)
-        res = f'{{"letter": "{letter}", "conf": "{conf}"}}'
+        print(type(conf), type(letter))
+        res = jsonify({"letter": letter, "conf": conf})
         return res
+
         
 
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(debug=True, port=5001)
